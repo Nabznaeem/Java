b162f90b19a969339d553fa0280fca3bd655bbdb
Nabznaeem <nabznaeem@yahoo.com>
2022-09-18 17:25:36 -0600

    First

---
+++ src/printName.java
@@ -0,0 +1,12 @@
+package src;
+//Write a program to print your name to console.
+import java.util.Scanner;
+
+public class printName {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String username= s.nextLine();
+        System.out.println("Name is "+ username);
+    }
+}
