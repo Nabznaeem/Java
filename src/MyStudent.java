ec3f1ed9cd94c5cd319ed7762d4e7dd2f4a061cc
Nabznaeem <nabznaeem@yahoo.com>
2022-09-18 17:05:31 -0600

    First time Using GIT

---
+++ src/MyStudent.java
@@ -0,0 +1,9 @@
+package src;
+
+public class MyStudent {
+    public static void main(String[] args) {
+        Student a= new Student();
+        a.setName("Sam");
+        System.out.println(a.getName());
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,18 @@
-public class Student {
-            public static void main(String[] args) {
+/*Create a class "Student.java" with a variable "Name". Write two functions:
 
-            System.out.println("Hello world");
+getName() that returns "Name" from state.
+setName() that takes an argument and updates the state variable "Name".*/
+package src;
+public class Student {
+
+        private String Name; // 0
+
+        public String getName(){
+            return  this.Name;
         }
-}
+
+        public void setName(String Name){
+            this.Name = Name;
+        }
+
+    }
