9c555f459aed9b837bbc9ef7241013675ab2b116
Nabznaeem <nabznaeem@yahoo.com>
2022-09-22 21:56:33 -0600

    Swap in array list

--- src/SecondWeek/ArrayL.java
+++ src/SecondWeek/ArrayL.java
@@ -7,24 +7,19 @@ import java.util.Collections;
 public class ArrayL {
     public static void main(String[] args) {
 
-            // Create the Array List
             ArrayList<String> Arr = new ArrayList<>();
 
-            // add the values in Array List
             Arr.add("Str 1");
             Arr.add("Str 2");
             Arr.add("Str 3");
             Arr.add("Str 4");
             Arr.add("Str 5");
 
-            // display Array List before swap
             System.out.println("Before swap the ArrayList ");
             System.out.println(Arr);
 
-            // Swapping the elements at 1 and 2 indices
             Collections.swap(Arr, 1, 2);
 
-            // display Array List after swap
             System.out.println("After swap the ArrayList");
             System.out.println(Arr);
         }
